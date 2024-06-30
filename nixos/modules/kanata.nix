3273eb0c3be35aa944c29ed0925b55d137815686
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2024-06-30 18:29:24 +0200

    remap caps lock to esc

---
+++ nixos/modules/kanata.nix
@@ -0,0 +1,15 @@
+# Remap Caps Lock to esc
+# https://github.com/jtroo/kanata/blob/main/docs/config.adoc#deflayermap
+{
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      # name of keyboard is arbitrary, works with all keyboards
+      "keychron".config = '' 
+        (defsrc)
+        (deflayermap (base-layer)
+          caps esc
+        )      '';
+    };
+  };
+}
