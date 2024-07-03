ca0bfb5a8f308e5023397522c43d43249bc05b55
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2024-07-03 16:40:25 +0200

    change font size

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -1,10 +1,10 @@
- {
+{
   programs.kitty = {
     enable = true;
     theme = "Catppuccin-Mocha";
     font = {
-    	name = "JetBrainsMono Nerd Font";
-	size = 18;
+      name = "FiraCode";
+      size = 15;
     };
   };
 }
