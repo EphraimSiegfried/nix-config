9ceceefaed575912bec044899cf91bdd54215a96
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2024-07-17 10:16:51 +0200

    add tokyo night sddm

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -1,13 +1,17 @@
+{ pkgs, ... }:
+let
+  tokyo-night-sddm = pkgs.libsForQt5.callPackage ./tokyo-night-sddm.nix { };
+in
 {
+  environment.systemPackages = with pkgs; [ tokyo-night-sddm ];
   services.xserver = {
     enable = true;
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
-      theme = "where_is_my_sddm_theme";
+      theme = "tokyo-night-sddm";
     };
   };
-
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
