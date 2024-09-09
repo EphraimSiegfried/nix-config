8c3ba4e3bbdfe527957daa3ff80f2de0ef3751c2
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2024-09-09 08:29:26 +0200

    add daemon which changes volume on network change

--- darwin/launchd.nix
+++ darwin/launchd.nix
@@ -4,6 +4,7 @@
       script = builtins.readFile ./netw-vol.sh;
       serviceConfig = {
         WatchPaths = [
+          # Files that change when changing networks
           /etc/resolv.conf
           /var/run/resolv.conf
           /private/var/run/resolv.conf
--- darwin/netw-vol.sh
+++ darwin/netw-vol.sh
@@ -1,16 +1,14 @@
-#!\bin\bash
-
-mute_ssid="LeSiegi"
+# This script sets the volume to 30% if at home and else mutes
+#
+home_ssid="siegi"
 
 get_ssid="$(networksetup -getairportnetwork en0 | sed -E 's,^Current Wi-Fi Network: (.+)$,\1,')"
 set_volume(){
   osascript -e "set volume output volume $1"
 }
 
-if [[ "${get_ssid}" == "$mute_ssid" ]]; then
-  set_volume 0
+if [[ "${get_ssid}" == "$home_ssid" ]]; then
+  set_volume 40
 else 
-  echo "${get_ssid}"""
-  set_volume 30
+  set_volume 0
 fi
-
