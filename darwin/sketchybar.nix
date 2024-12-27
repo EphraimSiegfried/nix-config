bef70300a0f05718b9d055167179a685e73ab027
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2024-12-27 14:53:43 +0100

    rewrite sketchybar

--- darwin/sketchybar.nix
+++ darwin/sketchybar.nix
@@ -1,9 +1,21 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  # TODO: call this through pkgs (does not work for some reason)
+  sbarlua = pkgs.callPackage ../pkgs/sbarlua { };
+
+in
+{
   services.sketchybar = {
     enable = true;
     extraPackages = with pkgs; [
-      jq
       sketchybar-app-font
+      wttrbar
+      sbarlua
+      lua5_4
     ];
   };
+  # launchd.user.agents.sketchybar.serviceConfig = {
+  #   StandardOutPath = "/tmp/sketchybar-logs/sketchybar.stdout.log";
+  #   StandardErrorPath = "/tmp/sketchybar-logs/sketchybar.stderr.log";
+  # };
 }
