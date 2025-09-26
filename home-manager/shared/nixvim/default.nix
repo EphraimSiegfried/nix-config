d6465c7c630cdc9c851f5201931de28646ba5bcb
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-09-26 13:03:23 +0200

    formatting

--- home-manager/shared/nixvim/default.nix
+++ home-manager/shared/nixvim/default.nix
@@ -1,4 +1,5 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+{
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
     ./config/keymaps.nix
--- home-manager/shared/ssh.nix
+++ home-manager/shared/ssh.nix
@@ -2,7 +2,6 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-
     matchBlocks = {
       "*" = {
         setEnv = {
