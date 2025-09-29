9c5c9876e3e11ab732cc8e7427cfdcfb4576657a
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-09-29 11:37:00 +0200

    improve config generation and add window workspace change triggers

--- darwin/aerospace.nix
+++ darwin/aerospace.nix
@@ -1,47 +1,49 @@
 { lib, pkgs, ... }:
+
+let
+  sketchybar = "${pkgs.sketchybar}/bin/sketchybar";
+  aerospace = "${pkgs.aerospace}/bin/aerospace";
+
+  genLisz = func: builtins.tail (builtins.genList (x: func (toString x)) 10);
+  genEnd = str: genLisz (x: "${str}${x}");
+
+  mkAttrSet =
+    keys: values: lib.listToAttrs (lib.zipListsWith (name: value: { inherit name value; }) keys values);
+in
 {
   services.aerospace = {
     enable = true;
     settings = {
-      mode.main.binding = {
-        alt-h = "focus left";
-        alt-j = "focus down";
-        alt-k = "focus up";
-        alt-l = "focus right";
-        alt-shift-h = "move left";
-        alt-shift-j = "move down";
-        alt-shift-k = "move up";
-        alt-shift-l = "move right";
-        alt-1 = "workspace 1";
-        alt-2 = "workspace 2";
-        alt-3 = "workspace 3";
-        alt-4 = "workspace 4";
-        alt-5 = "workspace 5";
-        alt-6 = "workspace 6";
-        alt-7 = "workspace 7";
-        alt-8 = "workspace 8";
-        alt-9 = "workspace 9";
-        alt-shift-1 = "move-node-to-workspace 1";
-        alt-shift-2 = "move-node-to-workspace 2";
-        alt-shift-3 = "move-node-to-workspace 3";
-        alt-shift-4 = "move-node-to-workspace 4";
-        alt-shift-5 = "move-node-to-workspace 5";
-        alt-shift-6 = "move-node-to-workspace 6";
-        alt-shift-7 = "move-node-to-workspace 7";
-        alt-shift-8 = "move-node-to-workspace 8";
-        alt-shift-9 = "move-node-to-workspace 9";
-        alt-tab = "workspace-back-and-forth";
-        alt-f = "fullscreen";
-        alt-shift-f = "fullscreen --no-outer-gaps";
-        # cmd-f = "macos-native-fullscreen";
-        alt-shift-minus = "resize smart -50";
-        alt-shift-equal = "resize smart +50";
-        alt-shift-semicolon = "mode service";
-        shift-cmd-k = "exec-and-forget ${lib.getExe pkgs.kitty} --single-instance -d ~";
-        shift-cmd-s = "exec-and-forget open /Applications/Spotify.app";
-        shift-cmd-b = "exec-and-forget open -n '/Applications/Zen Browser.app'";
+      mode.main.binding =
 
-      };
+        mkAttrSet (genEnd "alt-") (genEnd "workspace ")
+        // mkAttrSet (genEnd "alt-shift-") (
+          genLisz (x: [
+            "move-node-to-workspace ${x}"
+            "exec-and-forget ${sketchybar} --trigger change_window_workspace TARGET_WORKSPACE=${x} FOCUSED_WORKSPACE=$(${aerospace} list-workspaces --focused)"
+          ])
+        )
+        // {
+          alt-h = "focus left";
+          alt-j = "focus down";
+          alt-k = "focus up";
+          alt-l = "focus right";
+          alt-shift-h = "move left";
+          alt-shift-j = "move down";
+          alt-shift-k = "move up";
+          alt-shift-l = "move right";
+          alt-tab = "workspace-back-and-forth";
+          alt-f = "fullscreen";
+          alt-shift-f = "fullscreen --no-outer-gaps";
+          # cmd-f = "macos-native-fullscreen";
+          alt-shift-minus = "resize smart -50";
+          alt-shift-equal = "resize smart +50";
+          alt-shift-semicolon = "mode service";
+          shift-cmd-k = "exec-and-forget ${lib.getExe pkgs.kitty} --single-instance -d ~";
+          shift-cmd-s = "exec-and-forget open /Applications/Spotify.app";
+          shift-cmd-b = "exec-and-forget open -n '/Applications/Zen Browser.app'";
+
+        };
       mode.service.binding = {
         alt-shift-h = [
           "join-with left"
@@ -68,7 +70,7 @@
       exec-on-workspace-change = [
         "/bin/bash"
         "-c"
-        "/opt/homebrew/bin/sketchybar --trigger aerospace_workspace_change FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE PREV_WORKSPACE=$AEROSPACE_PREV_WORKSPACE"
+        "${sketchybar} --trigger aerospace_workspace_change FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE PREV_WORKSPACE=$AEROSPACE_PREV_WORKSPACE"
       ];
       gaps = {
         outer = {
