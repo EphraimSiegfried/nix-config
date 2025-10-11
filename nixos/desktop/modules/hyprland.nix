4f10f3a10748cc40c90ff2c7959ac061732360e6
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-10-11 16:14:40 +0200

    remove sddm

--- nixos/desktop/modules/hyprland.nix
+++ nixos/desktop/modules/hyprland.nix
@@ -1,19 +1,7 @@
-{ pkgs, ... }:
-let
-  tokyo-night-sddm = pkgs.libsForQt5.callPackage ./tokyo-night-sddm.nix { };
-in
 {
-  environment.systemPackages = with pkgs; [ tokyo-night-sddm ];
-  services.xserver = {
-    enable = true;
-    displayManager.sddm = {
-      enable = true;
-      wayland.enable = true;
-      theme = "tokyo-night-sddm";
-    };
-  };
   programs.hyprland = {
     enable = true;
+    withUWSM = true;
     xwayland.enable = true;
   };
 }
