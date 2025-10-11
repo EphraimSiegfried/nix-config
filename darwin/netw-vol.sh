5ad28cb55d8e0b6988831d493cb7c7ddf850fdb5
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-10-11 21:22:06 +0200

    update volume change script

--- darwin/netw-vol.sh
+++ darwin/netw-vol.sh
@@ -5,28 +5,26 @@ home_ssid="siegi"
 # TODO: change path
 last_ssid_file="Users/siegi/.last_ssid"
 
-get_ssid="$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')"
+current_ssid="$(ipconfig getsummary en0 | awk -F ' SSID : ' '/ SSID : / {print $2}')"
 
 # Read the last known SSID
+last_ssid=""
 if [[ -f "$last_ssid_file" ]]; then
-  last_ssid=$(cat "$last_ssid_file")
-else
-  last_ssid=""
+	last_ssid=$(cat "$last_ssid_file")
 fi
-
-set_volume(){
-  osascript -e "set volume output volume $1"
-}
 
 # Only trigger if the previous SSID was not "siegi"
 if [[ "$last_ssid" != "$home_ssid" ]]; then
-  cat "$get_ssid" >> "Users/siegi/.netw-vol-log"
-  if [[ "$get_ssid" == "$home_ssid" ]]; then
-    set_volume 40
-  else 
-    set_volume 0
-  fi
+	if [[ "$current_ssid" == "$home_ssid" ]]; then
+		set_volume 40
+	else
+		set_volume 0
+	fi
 fi
 
 # Save the current SSID for the next run
-echo "$get_ssid" > "$last_ssid_file"
+echo "$current_ssid" >"$last_ssid_file"
+
+set_volume() {
+	osascript -e "set volume output volume $1"
+}
