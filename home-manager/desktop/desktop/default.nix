fa3ee5ed4aa72596e86218570b71c69a74d324f7
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-10-11 16:11:50 +0200

    use other launcher

--- home-manager/desktop/desktop/default.nix
+++ home-manager/desktop/desktop/default.nix
@@ -1,20 +1,22 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
     wlogout # gui logout menu
     wl-clipboard
 
-    grim #takes screenshots
-    wf-recorder #takes screen recordings
-    slurp #can capture selected area
+    grim # takes screenshots
+    wf-recorder # takes screen recordings
+    slurp # can capture selected area
 
     pavucontrol
   ];
   imports = [
     ./hyprland.nix
-    ./swaync.nix
+    # ./swaync.nix
     ./waybar
+    # ./hyprpanel.nix
     ./hypridle.nix
-    ./wofi.nix
+    # ./wofi.nix
     ./hyprpaper.nix
     ./anyrun
   ];
