09d496e9452a8600bbd7988c35ea83c64cb3254c
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-10-11 22:58:59 +0200

    use meld as default merge tool

--- home-manager/shared/git.nix
+++ home-manager/shared/git.nix
@@ -1,9 +1,22 @@
+{ pkgs, ... }:
 {
   programs.git = {
     enable = true;
     userEmail = "ephraim.siegfried@hotmail.com";
     userName = "Ephraim Siegfried";
+    extraConfig = {
+      merge = {
+        tool = "nvimdiff2";
+        guitool = "meld";
+      };
+      mergetool = {
+        # 'auto' makes git use guitool if display is available
+        # guiDefault = "auto";
+        guiDefault = true;
+      };
+    };
   };
   programs.gh.enable = true;
   programs.lazygit.enable = true;
+  home.packages = [ pkgs.meld ];
 }
