2b9e10bbba68982186f6afe38d0c933ef14a6a8f
Ephraim Siegfried <ephraim.siegfried@proton.me>
2025-11-11 07:36:11 +0100

    add desktop ssh key

--- nixos/common/users.nix
+++ nixos/common/users.nix
@@ -17,6 +17,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDqWs4UGEkx+HwmzymPMSyBshtygcza0ov9u8uuLGPbH"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1R2gEuXslK413gWBE4tOA894zO/MkhZrAK/LyRcsmo"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGdANrCkeXTrZha/w3pvg/vCZWmuRsy7cI6PmgVfWH8c siegi@blinkybill"
     ];
   };
 }
