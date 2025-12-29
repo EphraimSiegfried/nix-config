690c1493f11a65ae408a5e2d03701f2b0eafecc8
Ephraim Siegfried <ephraim.siegfried@proton.me>
2025-12-29 17:19:16 +0100

    fix nixpkgs version mismatch

--- flake.nix
+++ flake.nix
@@ -4,18 +4,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur.url = "github:nix-community/NUR";
-    darwin.url = "github:LnL7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix";
+
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     disko = {
       url = "github:nix-community/disko";
@@ -30,6 +33,9 @@
     vicinae = {
       url = "github:vicinaehq/vicinae";
     };
+
+    sops-nix.url = "github:Mic92/sops-nix";
+    nur.url = "github:nix-community/NUR";
   };
 
   outputs =
