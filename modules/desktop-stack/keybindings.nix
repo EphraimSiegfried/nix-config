71055961a86c1ca01717fce9be3712a4892bbfb8
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-03-07 13:07:17 +0100

    map function keys to usual operations

--- modules/desktop-stack/keybindings.nix
+++ modules/desktop-stack/keybindings.nix
@@ -60,6 +60,16 @@
             ) 10
           )
         );
+      bindel = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+        ", XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+      ];
     };
   };
 
