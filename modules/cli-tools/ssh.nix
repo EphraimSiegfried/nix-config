d63dc9cd044248f25cbed066d46651b2f0efa121
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-03-27 07:51:26 +0100

    add ssh hosts

--- modules/cli-tools/ssh.nix
+++ modules/cli-tools/ssh.nix
@@ -4,6 +4,26 @@
       enable = true;
       enableDefaultConfig = false;
       matchBlocks = {
+        gitlab-tds = {
+          host = "gitlab-tds";
+          hostname = "madra-test.cscs.ch";
+          user = "root";
+        };
+        gitlab-dev = {
+          host = "gitlab-dev";
+          hostname = "madra-dev.cscs.ch";
+          user = "root";
+        };
+        confluence-tds = {
+          host = "confluence-tds";
+          hostname = "ascona-tds.cscs.ch";
+          user = "root";
+        };
+        jira-tds = {
+          host = "jira-tds";
+          hostname = "agra-tds.cscs.ch";
+          user = "root";
+        };
         "*" = {
           setEnv = {
             "TERM" = "xterm-256color";
