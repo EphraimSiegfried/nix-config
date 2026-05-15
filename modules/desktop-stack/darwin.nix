016d0aeff2d02aee7d19d347948450d3475df037
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-05-15 19:03:41 +0200

    fix missing sketchybar

--- modules/desktop-stack/darwin.nix
+++ modules/desktop-stack/darwin.nix
@@ -24,6 +24,21 @@
     };
   };
 
+  flake.modules.darwin.sketchybar =
+    { pkgs, ... }:
+    {
+      homebrew.brews = [ "ifstat" ];
+      homebrew.casks = [ "sf-symbols" ];
+      services.sketchybar = {
+        enable = true;
+        package = inputs.sketchybar-config.packages.${pkgs.system}.default;
+      };
+      launchd.user.agents.sketchybar.serviceConfig = {
+        StandardOutPath = "/tmp/sketchybar-logs/sketchybar.stdout.log";
+        StandardErrorPath = "/tmp/sketchybar-logs/sketchybar.stderr.log";
+      };
+    };
+
   flake.modules.darwin.system_defaults = {
     system = {
       defaults = {
