acc5b0ef7bfa99dcb7f55cb405a2ef607ad11547
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-05-17 08:24:43 +0200

    improve nautilus

--- modules/gui-apps/nautilus.nix
+++ modules/gui-apps/nautilus.nix
@@ -2,12 +2,18 @@
   flake.modules.nixos.nautilus =
     { pkgs, ... }:
     {
-      environment.systemPackages = [
-        pkgs.nautilus
-        pkgs.loupe
+      environment.systemPackages = with pkgs; [
+        nautilus
+        loupe
+        evince
+        ffmpegthumbnailer
+        gnome-font-viewer
+        webp-pixbuf-loader
+        librsvg
       ];
       services.gvfs.enable = true;
-
+      services.gnome.sushi.enable = true;
+      environment.pathsToLink = [ "/share/thumbnailers" ];
       nixpkgs.overlays = [
         (final: prev: {
           nautilus = prev.nautilus.overrideAttrs (nprev: {
